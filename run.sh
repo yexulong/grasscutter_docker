78ae19e4dd7a24b7e552417bb82d6602873abca2
yexulong <yexulong@foxmail.com>
2022-05-07 14:34:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,4 @@ apt-get install -y p7zip
 
 7za x full-grasscutter-dev-372_TomyJan.7z
 
-mv *docker* full-grasscutter-dev-372_TomyJan
-
-cd full-grasscutter-dev-372_TomyJan
-
 docker-compose -f genimpact-docker-compose.yml up -d --build
